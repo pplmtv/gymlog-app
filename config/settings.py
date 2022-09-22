eaa4b0882c4e6b4a9fbf44052697d360c8b88582
pplmtv <pplmtv@gmail.com>
2022-09-22 12:45:28 +0900

    settings.py fixed

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = env.get_value('SECRET_KEY', str)
 # DEBUG = True
 DEBUG = False
 
-ALLOWED_HOSTS = ['pplmtv-gymlog.link']
+# ALLOWED_HOSTS = ['pplmtv-gymlog.link']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
