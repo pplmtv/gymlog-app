6189ae81e9d3b4ca001a866ea5573b7305e67834
pplmtv <pplmtv@gmail.com>
2022-11-20 13:58:08 +0900

    settings.py fixed

--- config/settings.py
+++ config/settings.py
@@ -59,9 +59,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
